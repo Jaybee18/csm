155b5a0dfe4051f2060a3b6e2404bb1209b9a12c
Jan Bessler <mister.jaybee18@gmail.com>
2024-10-29 20:50:58 +0100

    added version flag

--- cmd/gocsm/main.go
+++ cmd/gocsm/main.go
@@ -23,9 +23,7 @@ examples and usage of using your application. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Version: "0.1.0",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -40,15 +38,9 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gocsm.yaml)")
 	rootCmd.PersistentFlags().StringSliceVar(&shell, "shell", []string{"/bin/sh", "-c"}, "shell for commands")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
 	InitCommand()
@@ -70,10 +62,6 @@ func initConfig() {
 		viper.SetConfigName(".gocsm")
 	}
 
-	// viper.AutomaticEnv() // read in environment variables that match
-
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		// fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
+	_ = viper.ReadInConfig()
 }
